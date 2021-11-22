7e1448b31138c170dd63f33109e823ab91847914
zgyzgyhero <894526647@qq.com>
2021-11-22 17:04:32 +0800

    Update file server

--- file_server/minio.go
+++ file_server/minio.go
@@ -17,17 +17,11 @@ type MinioServer struct {
 	Client        *minio.Client `json:"client"`
 }
 
-type MinioUploadParam struct {
+type MinioParam struct {
 	Ctx         context.Context `json:"ctx"`
 	Bucket      string          `json:"bucket"`
 	ObjectName  string          `json:"object_name"`
 	FileContent []byte          `json:"file_content"`
-}
-
-type MinioDownloadParam struct {
-	Ctx        context.Context `json:"ctx"`
-	Bucket     string          `json:"bucket"`
-	ObjectName string          `json:"object_name"`
 }
 
 func (m *MinioServer) Init() error {
@@ -41,7 +35,7 @@ func (m *MinioServer) Init() error {
 	return err
 }
 
-func (m *MinioServer) Upload(param MinioUploadParam) error {
+func (m *MinioServer) Upload(param MinioParam) error {
 	err := m.Client.MakeBucket(param.Ctx, param.Bucket, minio.MakeBucketOptions{Region: ""})
 	if err != nil {
 		exists, errBucketExists := m.Client.BucketExists(param.Ctx, param.Bucket)
@@ -61,7 +55,7 @@ func (m *MinioServer) Upload(param MinioUploadParam) error {
 	return nil
 }
 
-func (m *MinioServer) Download(param MinioDownloadParam) (result []byte, err error) {
+func (m *MinioServer) Download(param MinioParam) (result []byte, err error) {
 	obj, getErr := m.Client.GetObject(param.Ctx, param.Bucket, param.ObjectName, minio.GetObjectOptions{})
 	if getErr != nil {
 		err = fmt.Errorf("Download file fail,%s ", getErr.Error())
@@ -72,4 +66,12 @@ func (m *MinioServer) Download(param MinioDownloadParam) (result []byte, err err
 		err = fmt.Errorf("Read file content fail,%s ", err.Error())
 	}
 	return result, err
+}
+
+func (m *MinioServer) Remove(param MinioParam) error {
+	err := m.Client.RemoveObject(param.Ctx, param.Bucket, param.ObjectName, minio.RemoveObjectOptions{ForceDelete: true})
+	if err != nil {
+		err = fmt.Errorf("Remove file:%s/%s fail,%s ", param.Bucket, param.ObjectName, err.Error())
+	}
+	return err
 }
